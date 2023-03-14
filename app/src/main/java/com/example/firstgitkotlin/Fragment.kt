0d882bac21e87ca59f4cb03ac2cfcea4f80ac05c
Aigerim-2801 <aygerim.berdimurat.03@gmail.com>
2023-03-14 10:40:49 +0600

    Inital commit

--- app/src/main/java/com/example/firstgitkotlin/Fragment.kt
+++ app/src/main/java/com/example/firstgitkotlin/Fragment.kt
@@ -5,21 +5,21 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.firstgitkotlin.databinding.Fragment1LayoutBinding
+import com.example.firstgitkotlin.databinding.FragmentLayoutBinding
 import com.example.firstgitkotlin.json.SemestersItem
 import java.io.Serializable
 
 
-class Fragment1 : Fragment(R.layout.fragment1_layout) {
+class Fragment1 : Fragment(R.layout.fragment_layout) {
 
-    private var _binding: Fragment1LayoutBinding? = null
+    private var _binding: FragmentLayoutBinding? = null
     private val binding get() = _binding!!
 
     private val itemAdapter: ItemAdapter = ItemAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = Fragment1LayoutBinding.bind(view)
+        _binding = FragmentLayoutBinding.bind(view)
 
         val sem = arguments?.getSerializableCompat("semestr", SemestersItem::class.java)
 
